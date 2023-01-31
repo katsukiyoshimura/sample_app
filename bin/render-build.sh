863572d9df7dc44b3cfe0fab1aa118c5a1255eda
katsukiyoshimura <yoshimura@smartworx.co.jp>
2023-01-31 06:44:36 +0000

    add last sentence to bin

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,4 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate:reset
 bundle exec rails db:seed
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
